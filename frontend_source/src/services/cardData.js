fad35dd5c4ee552fd883d5a498c78f653f4439b3
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2022-11-04 10:52:22 +0200

    fixes hopefully

--- frontend_source/src/services/cardData.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = '/api/cards'
-
-const getCardData = async () => {
-  const response = await axios.get(baseUrl)
-  return response.data
-}
-
-export default { getCardData }
